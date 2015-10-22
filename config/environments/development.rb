5c7f033cfd1ecc81852fad4c93e1f88c29f5e654
Ralf Schmitz Bongiolo <ralfsb2008@gmail.com>
2015-10-22 02:28:38 +0000

    Requrie sprockets again; require 'trailblazer/autoload', so our concepts folder can work properly; Set some web-console and better_errors ip whitelist.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  #
+  config.web_console.whitelisted_ips = '10.0.2.2'
+  BetterErrors::Middleware.allow_ip! '10.0.2.2'
 end
